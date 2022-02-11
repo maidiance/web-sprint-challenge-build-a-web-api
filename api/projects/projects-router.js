188c9fa39368185d7977cf69801c282ca2ddf99a
Winifred Lu <winnie.lu@comcast.net>
2022-02-11 08:57:22 -0500

    debugged and refactored basic get endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,13 +1,31 @@
 // Write your "projects" router here!
 const express = require('express');
 const Projects = require('./projects-model');
-const { validateId } = require('/projects-middleware');
+const { validateId } = require('./projects-middleware');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const projects = Projects.get();
+    let projects = [];
+    Projects.get()
+        .then(resp => {
+            projects = resp;
+        })
+        .catch(() => {
+            res.status(500).json({message: 'failed to get projects'});
+        })
     res.status(200).json(projects);
+});
+
+router.get('/:id', validateId, (req, res) => {
+    const { id } = req.params;
+    Projects.get(id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(() => {
+            res.status(500).json({message: 'failed to get project'});
+        })
 });
 
 module.exports = router;
