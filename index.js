3c94a43167d4836306b158c5de6e47940744f5c0
Winifred Lu <winnie.lu@comcast.net>
2022-02-11 08:30:43 -0500

    basic server start done

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const express = require('express');
+const server = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 9000;
+
+server.use(express.json());
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
