43fab164985a3ea42e02e2b80906cb6fa1a646dc
Winifred Lu <winnie.lu@comcast.net>
2022-02-11 13:53:13 -0500

    action get endpoint complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,5 +4,18 @@ const Actions = require('./actions-model.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(resp => {
+            if(resp == null) {
+                res.status(200).json([]);
+            } else {
+                res.status(200).json(resp);
+            }
+        })
+        .catch(() => {
+            res.status(500).json({message: 'failed to get actions'});
+        })
+});
 
 module.exports = router;
